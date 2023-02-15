e167a550bc0c6ef040c9dd071dfd1625dabea812
[weekend79] <[mortenviken@yahoo.com]>
2023-02-15 21:21:18 +0100

    Updated view to save user

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
@@ -10,6 +11,10 @@ def home(request):
 def sign_up(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
     else:
         form = RegisterForm()
 
