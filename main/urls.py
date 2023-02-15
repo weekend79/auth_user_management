db8f60a0439a3da5f151ddc5b51708eda0e896e7
[weekend79] <[mortenviken@yahoo.com]>
2023-02-15 21:20:40 +0100

    Added sign-up url

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('sign-up', views.sign_up, name='sign_up')
+    path('sign-up', views.sign_up, name='sign_up'),
 ]
