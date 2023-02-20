07950c4f1c2a1a839e6e3ea05c93c9f75fda9d73
[weekend79] <[mortenviken@yahoo.com]>
2023-02-20 19:54:09 +0100

    Fix bug in the delete post

--- main/views.py
+++ main/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
+from .models import Post
 
 
 
 # Create your views here.
 @login_required(login_url="/login")
 def home(request):
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            post and (post.author == request.user)
+            post.delete()
+
+    return render(request, 'main/home.html', {"posts": posts})
 
 @login_required(login_url="/login")
 def create_post(request):
